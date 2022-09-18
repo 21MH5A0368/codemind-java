b86b6060647fafc73bdf9fc6da89a1eaa08e51cd
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2022-09-19 00:08:21 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],n,a,i,b,r,c=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         a=sc.nextInt();         b=sc.nextInt();         r=b;         for(i=0;i<n;i++)         {             if( x[i]>=a && x[i]<=b )             {                 c++;                 if(x[i]<r)                 {                     r=x[i];                 }             }         }         if(c>0)             System.out.println(r);         else             System.out.println("-1");         sc.close();     } }

---
+++ minimum_between_A_and_B.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x[],n,a,i,b,r,c=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        a=sc.nextInt();
+        b=sc.nextInt();
+        r=b;
+        for(i=0;i<n;i++)
+        {
+            if( x[i]>=a && x[i]<=b )
+            {
+                c++;
+                if(x[i]<r)
+                {
+                    r=x[i];
+                }
+            }
+        }
+        if(c>0)
+            System.out.println(r);
+        else
+            System.out.println("-1");
+        sc.close();
+    }
+}
