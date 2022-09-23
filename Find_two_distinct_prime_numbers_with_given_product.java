dc4e3ecc8c29b9509f636792aad8828da6b091ce
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2022-09-23 14:43:10 +0530

    import java.util.Scanner; class sample {     public static boolean isPrime(int n)     {         int i,count=0;         if(n<2)             return false;         for(i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)                 count++;         }         if(count==0)             return true;         else             return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,j,a,b,c=0,flag=0;         n=sc.nextInt();         for(i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             {                 if(isPrime(i) && isPrime(n/i))                 {                     flag=1;                     break;                 }             }         }         if(flag==1)            System.out.println(i+" "+n/i);         else            System.out.println("-1");
        }
    }

---
+++ Find_two_distinct_prime_numbers_with_given_product.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+class sample
+{
+    public static boolean isPrime(int n)
+    {
+        int i,count=0;
+        if(n<2)
+            return false;
+        for(i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+                count++;
+        }
+        if(count==0)
+            return true;
+        else
+            return false;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j,a,b,c=0,flag=0;
+        n=sc.nextInt();
+        for(i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                if(isPrime(i) && isPrime(n/i))
+                {
+                    flag=1;
+                    break;
+                }
+            }
+        }
+        if(flag==1)
+           System.out.println(i+" "+n/i);
+        else
+           System.out.println("-1");
+        
+    }
+}
+
+
+
