4bb7f9735fe779844c3225f0d4f145509337a2f9
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2022-09-25 01:05:10 +0530

    import java.util.Scanner; class sample {     public static boolean isperfect(int n)     {         int i,sum=0;         for(i=1;i<=n/2;i++)         {             if(n%i==0)             {                 sum=sum+i;             }         }         if(sum==n)             return true;         else             return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         if(isperfect(n))             System.out.println("True");         else             System.out.println("False");     } }

---
+++ Perfect_Number.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class sample
+{
+    public static boolean isperfect(int n)
+    {
+        int i,sum=0;
+        for(i=1;i<=n/2;i++)
+        {
+            if(n%i==0)
+            {
+                sum=sum+i;
+            }
+        }
+        if(sum==n)
+            return true;
+        else
+            return false;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        if(isperfect(n))
+            System.out.println("True");
+        else
+            System.out.println("False");
+    }
+}
