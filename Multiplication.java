4fb182fb4a8f5f965222b9227381e426adb57389
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2022-09-29 23:54:46 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,m;         a=sc.nextInt();         b=sc.nextInt();         sc.close();         m=a*b;         System.out.println(m);     } }

---
+++ Multiplication.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,m;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        sc.close();
+        m=a*b;
+        System.out.println(m);
+    }
+}
