5e7ed0b0ee1f32674880e67ac78dc877aa0ada0e
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2022-09-30 23:02:48 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int m,n;         m=sc.nextInt();         n=sc.nextInt();         sc.close();         System.out.println(m%n);     } }

---
+++ Modulus.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int m,n;
+        m=sc.nextInt();
+        n=sc.nextInt();
+        sc.close();
+        System.out.println(m%n);
+    }
+}
