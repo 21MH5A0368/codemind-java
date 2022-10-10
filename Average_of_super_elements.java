19669cd6d08f352a1ba6a41ca0f0bfbe07efbcd1
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2022-10-11 00:36:14 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int i,j,x[],n,count,c=0,flag=0;         float avg,sum=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         sc.close();         for(i=0;i<n;i++)         {             count=1;             if(x[i]!=-99)             {                 for(j=0;j<n;j++)                 {                     if(x[i]==x[j] &&i!=j)                     {                         count++;                         x[j]=-99;                     }                 }             }             if(x[i]==count)             {                 sum=sum+x[i];                 c++;                 flag=1;             }         }         avg=sum/c;         if(flag==1)             System.out.format("%.2f",avg);         else             System.out.println("-1");     } }

---
+++ Average_of_super_elements.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,j,x[],n,count,c=0,flag=0;
+        float avg,sum=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        sc.close();
+        for(i=0;i<n;i++)
+        {
+            count=1;
+            if(x[i]!=-99)
+            {
+                for(j=0;j<n;j++)
+                {
+                    if(x[i]==x[j] &&i!=j)
+                    {
+                        count++;
+                        x[j]=-99;
+                    }
+                }
+            }
+            if(x[i]==count)
+            {
+                sum=sum+x[i];
+                c++;
+                flag=1;
+            }
+        }
+        avg=sum/c;
+        if(flag==1)
+            System.out.format("%.2f",avg);
+        else
+            System.out.println("-1");
+    }
+}
