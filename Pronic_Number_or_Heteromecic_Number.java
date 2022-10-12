245eb5d6b165ed4d1f6ee66740244fd7bed888aa
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2022-10-13 01:38:26 +0530

    import java.util.Scanner; class sample {     public static boolean ispronic(int n)     {         int x;         x=(int)Math.sqrt(n);         if(x*(x+1)==n)             return true;         else             return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         sc.close();         if(ispronic(n))             System.out.println("YES");         else             System.out.println("NO");     } }

---
+++ Pronic_Number_or_Heteromecic_Number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class sample
+{
+    public static boolean ispronic(int n)
+    {
+        int x;
+        x=(int)Math.sqrt(n);
+        if(x*(x+1)==n)
+            return true;
+        else
+            return false;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        sc.close();
+        if(ispronic(n))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
