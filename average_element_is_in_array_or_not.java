27386401736c93d22e971f5b61a661208843d0b8
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2022-11-04 22:33:33 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],n,i,avg,sum=0,c=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         sc.close();         for(i=0;i<n;i++)         {             sum=sum+x[i];         }         avg=(sum/n);         for(i=0;i<n;i++)         {             if(avg==x[i])             {               c++;             }         }         if(c!=0)             System.out.println("True");         else             System.out.println("False");     } }

---
+++ average_element_is_in_array_or_not.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x[],n,i,avg,sum=0,c=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        sc.close();
+        for(i=0;i<n;i++)
+        {
+            sum=sum+x[i];
+        }
+        avg=(sum/n);
+        for(i=0;i<n;i++)
+        {
+            if(avg==x[i])
+            {
+              c++;
+            }
+        }
+        if(c!=0)
+            System.out.println("True");
+        else
+            System.out.println("False");
+    }
+}
