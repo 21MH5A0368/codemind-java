a2daed2ba097df860d64ab86b1f28057bde30532
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2022-11-06 22:18:20 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],n,i,a=0,b=0,c=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         sc.close();         for(i=0;i<n;i++)         {             if(x[i]%2==1)             {                 a=i;                 break;             }         }         for(i=n-1;i>=0;i--)         {             if(x[i]%2==1)             {                 b=i;                 break;             }         }         for(i=a;i<b;i++)         {             if(x[i]%2==0)             {                 c++;             }         }         System.out.println(c);     } }

---
+++ count_even_numbers_between_odd_numbers.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x[],n,i,a=0,b=0,c=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        sc.close();
+        for(i=0;i<n;i++)
+        {
+            if(x[i]%2==1)
+            {
+                a=i;
+                break;
+            }
+        }
+        for(i=n-1;i>=0;i--)
+        {
+            if(x[i]%2==1)
+            {
+                b=i;
+                break;
+            }
+        }
+        for(i=a;i<b;i++)
+        {
+            if(x[i]%2==0)
+            {
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
