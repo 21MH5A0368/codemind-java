5b9783d1a286bc7907f703be8a45913edab1192b
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2022-12-08 23:09:13 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int i,count=0,x[],n,c=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         sc.close();         for(i=0;i<n;i++)         {             if(x[i]%2!=0 && i%2!=0)             {                 count++;             }             if(x[i]%2!=0)             {                 c++;             }         }         if(c==count)             System.out.println("True");         else             System.out.println("False");     } }

---
+++ Strictly_ODD.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,count=0,x[],n,c=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        sc.close();
+        for(i=0;i<n;i++)
+        {
+            if(x[i]%2!=0 && i%2!=0)
+            {
+                count++;
+            }
+            if(x[i]%2!=0)
+            {
+                c++;
+            }
+        }
+        if(c==count)
+            System.out.println("True");
+        else
+            System.out.println("False");
+    }
+}
