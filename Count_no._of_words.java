0134dcc64c8523c47d40979d540be06dc34f4c58
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2022-12-12 21:27:50 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String str=sc.nextLine();         sc.close();         int i,c=0;         for(i=0;i<str.length();i++){             char ch=str.charAt(i);             if( ch==' ' || i==str.length()-1){                 c++;             }         }         System.out.print(c);     } }

---
+++ Count_no._of_words.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        sc.close();
+        int i,c=0;
+        for(i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if( ch==' ' || i==str.length()-1){
+                c++;
+            }
+        }
+        System.out.print(c);
+    }
+}
