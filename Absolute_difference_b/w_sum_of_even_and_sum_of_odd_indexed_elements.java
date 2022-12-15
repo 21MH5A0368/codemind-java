f903b6f0bf37545d1d401b50267e7311e83d64cd
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2022-12-15 22:46:21 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],n,es=0,os=0,i,d;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         sc.close();         for(i=0;i<n;i++)         {             if(i%2==0)             {                 es=es+x[i];             }             else             {                 os=os+x[i];             }         }         d=Math.abs(es-os);         System.out.print(d);     } }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x[],n,es=0,os=0,i,d;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        sc.close();
+        for(i=0;i<n;i++)
+        {
+            if(i%2==0)
+            {
+                es=es+x[i];
+            }
+            else
+            {
+                os=os+x[i];
+            }
+        }
+        d=Math.abs(es-os);
+        System.out.print(d);
+    }
+}
