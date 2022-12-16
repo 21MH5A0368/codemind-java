cf6ca2277a5f75dff82ae58c0ce23f95b40ae5a5
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2022-12-16 22:55:04 +0530

    import java.util.Scanner; class sample { 	public static boolean isprime(int n) 	{ 	    if(n<2) 	        return false; 		int count=0; 		for(int i=2;i<=(int)Math.sqrt(n);i++) 		{ 			if(n%i==0) 			{ 				count++; 				break; 			} 		} 		if(count==0) 			return true; 		else 			return false; 	} 	public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int i,j,x[],n,c=0;         float avg,sum=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         sc.close();         for(i=0;i<n;i++)         {             if(isprime(x[i]))             {                 c++;                 sum+=x[i];             }         }         avg=sum/c;         System.out.format("%.2f",avg); 	} }

---
+++ Average_of_primes.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+class sample
+{
+	public static boolean isprime(int n)
+	{
+	    if(n<2)
+	        return false;
+		int count=0;
+		for(int i=2;i<=(int)Math.sqrt(n);i++)
+		{
+			if(n%i==0)
+			{
+				count++;
+				break;
+			}
+		}
+		if(count==0)
+			return true;
+		else
+			return false;
+	}
+	public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,j,x[],n,c=0;
+        float avg,sum=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        sc.close();
+        for(i=0;i<n;i++)
+        {
+            if(isprime(x[i]))
+            {
+                c++;
+                sum+=x[i];
+            }
+        }
+        avg=sum/c;
+        System.out.format("%.2f",avg);
+	}
+}
