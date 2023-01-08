11639a5b338d02f08ca58546ef75a242bfd587ee
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2023-01-08 22:54:20 +0530

    import java.util.Scanner; class sample{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int vc=0,max=0;         String s=sc.nextLine();         sc.close();         String v="AEIOUaeiou";         String words[]=s.split(" ");         for(int i=0;i<words.length;i++){             for(int j=0;j<words[i].length();j++){                 if(v.contains(words[i].charAt(j)+"")){                     vc++;                 }             }             if(vc>max){                 max=vc;             }             vc=0;         }         System.out.print(max);     } }

---
+++ maximum_vowel_count.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class sample{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int vc=0,max=0;
+        String s=sc.nextLine();
+        sc.close();
+        String v="AEIOUaeiou";
+        String words[]=s.split(" ");
+        for(int i=0;i<words.length;i++){
+            for(int j=0;j<words[i].length();j++){
+                if(v.contains(words[i].charAt(j)+"")){
+                    vc++;
+                }
+            }
+            if(vc>max){
+                max=vc;
+            }
+            vc=0;
+        }
+        System.out.print(max);
+    }
+}
