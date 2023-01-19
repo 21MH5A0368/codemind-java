85b3baf9b9e6c9b75af5437249bc523aafce82d0
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2023-01-19 16:41:03 +0530

    import java.util.Scanner; class sample{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         int i,count=0;         for(i=0;i<s.length();i++){             if(s.charAt(i)>=65 && s.charAt(i)<=90)             {                 count++;             }         }         System.out.println(count);     } }

---
+++ uppercase_letters_in_a_string.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class sample{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int i,count=0;
+        for(i=0;i<s.length();i++){
+            if(s.charAt(i)>=65 && s.charAt(i)<=90)
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
