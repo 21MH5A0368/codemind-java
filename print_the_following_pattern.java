738bba9ea20ce20f9e5721616d6209a91f00b9ae
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2023-01-25 12:27:03 +0530

    import java.util.Scanner; class sample{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         for(int i=0 ; i < n ; i++ ){             for(int j=0;j<n;j++){                 if( j==0 || j==i || j==n-1){                     System.out.print("* ");                 }                 else{                     System.out.print("  ");                 }             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
-class sample
-{
-    public static void main(String args[])
-    {
+class sample{
+    public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        int n,i,j;
+        int n;
         n=sc.nextInt();
-        for(i=1;i<=n;i++)
-        {
-            for(j=1;j<=n;j++)
-            {
-                if(i==j)
-                    System.out.print("0");
-                else
-                    System.out.print("x");
+        for(int i=0 ; i < n ; i++ ){
+            for(int j=0;j<n;j++){
+                if( j==0 || j==i || j==n-1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
             }
             System.out.println();
         }
-        sc.close();
     }
 }
